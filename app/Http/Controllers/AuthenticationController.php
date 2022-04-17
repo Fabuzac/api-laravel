626432a914343b3d97d938768b3b6234eaaa1df4
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-04-17 10:49:45 +0200

    route api for -> app-react

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request, $user)
+    {
+        dd($request->all());
+        User::create([
+            'email' => $request->input('email'),
+            'name' => $request->input('name'),
+            'name' => $request->input('password'),
+            // 'password' => bcrypt($request->input('password')),
+            // 'api_token' => Str::random(60),
+        ]);
+
+        return response()->json($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+// Route::post('/register', [AuthenticationController::class, 'register']);
 
-Route::get('/users', function () {
-   $user = new stdClass();
-   $user->name = 'Nove';
-   $user->email = 'test@test.com';
-   return response()->json($user);
+Route::get('/test', function() {
+   return 'test';
 });
